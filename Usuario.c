ee6af096384261e26d657c4aef9f8d728847191d
noteperdononiki <agustinlo035@gmail.com>
2026-06-18 18:24:16 -0300

    cambios en tipo de dato (scanf) que recibe

--- Usuario.c
+++ Usuario.c
@@ -261,18 +261,6 @@ void deshacerUltimaCompra(Pila *historialId, Usuario *usuarioAReembolsarJuego)
 
             quitarJuegoDeBibliotecaUsuario(&(*usuarioAReembolsarJuego).bibliotecaUsuario, &(*usuarioAReembolsarJuego).validosBiblioteca, juegoAQuitar);
 
-=======
-        //pila es una struct con posiciones para su dim
-
-//        (*historialId).postope;
-
-        if(ultimoJuegoComprado.id != -1)
-        {
-            float montoAReembolsar = juegoAQuitar.precioJuego;
-
-            quitarJuegoDeBibliotecaUsuario(&(*usuarioAReembolsarJuego).bibliotecaUsuario, &(*usuarioAReembolsarJuego).validosBiblioteca, juegoAQuitar);
-
->>>>>>> 0201745f9a053a126282d0804e018ee1e80708ca
             (*usuarioAReembolsarJuego).billetera += montoAReembolsar;
         }
         else
